19b868d49d9ff8e75895a3319dad9bedb9200573
LoganWantstoCode <logan.landen95@yahoo.com>
2023-03-22 20:23:13 -0500

    server and some starting code

---
+++ Develop/routes/comments.js
@@ -0,0 +1,29 @@
+const Comments = require('express').Router();
+const { readFromFile, readAndAppend } = require('../utils/fsUtils.js');
+const uuid = require('../helpers/uuid');
+
+Comments.get('/', (req, res) => {
+  readFromFile('./db/tips.json').then((data) => res.json(JSON.parse(data)));
+});
+
+Comments.post('/', (req, res) => {
+  console.log(req.body);
+
+  const { username, topic, tip } = req.body;
+
+  if (req.body) {
+    const newComment = {
+      username,
+      tip,
+      topic,
+        comment_id: uuid(),
+    };
+
+    readAndAppend(newComment, './db/CHANGEME*********');
+    res.json(`Comment added successfully 🚀`);
+  } else {
+    res.error('Error in adding comment');
+  }
+});
+
+module.exports = Comments;
--- Develop/server.js
+++ Develop/server.js
@@ -11,7 +11,15 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Set up Handlebars.js engine with custom helpers
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+  );
+
+  app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/pages/reviews.html'))
+  ); 
 const hbs = exphbs.create({ helpers });
 
 const sess = {
