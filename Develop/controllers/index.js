429e32b99ea9bcb88d2265034fcc42c728689e8b
Luis Martinez <luismtz@Luiss-MacBook-Air.local>
2023-03-22 20:49:42 -0500

    got the server to work

--- Develop/controllers/index.js
+++ Develop/controllers/index.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+// This will be the api for our items
+const productRoutes = require('./productRoutes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
