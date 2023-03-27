3739808a56694f930d542808a1c1bfa7e7992879
Luis Martinez <luismtz@Luiss-MacBook-Air.local>
2023-03-27 18:59:11 -0500

    added the handleBars

--- Develop/controllers/index.js
+++ Develop/controllers/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
-// const homeRoutes = require('./homeRoutes');
+const homeRoutes = require('./homeRoutes');
 // This will be the api for our items
 const productRoutes = require('./productRoutes');
 
-// router.use('/', homeRoutes);
+ router.use('/home', homeRoutes);
 router.use('/', productRoutes)
 router.use('/api', apiRoutes);
 
